56d7454213c13932398021e9e653c405ecf53b74
Rick Ansay <rick.ansay@gmail.com>
2021-11-18 23:19:20 -0700

    More updates

--- Assets/script.js
+++ Assets/script.js
@@ -3,10 +3,18 @@
 // Display current date at top of calendar
 let currentDay = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
 
+$('#currentDay').text(currentDay);
 // Below are timeblocks for standard business hours.
-// Timeblocks must be color coded for past/present/future
+// Timeblocks must be color coded for past/present/future time slots
 // Save events into timeblocks, must stay after pages refreshes
 
-$('#currentDay').text(currentDay);
+function currentTime() {
+    let currentHour = moment().hours();
+    let time = $('.time-block');
 
-console.log(currentDay);
+    time.each(function(){
+        let hour = parseInt($(this).attr('id'))
+
+
+    })
+}
