8c070861a5efb6a868673c1f333555844cdaa4a3
Rick Ansay <rick.ansay@gmail.com>
2021-11-21 13:41:20 -0700

    Added JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,15 @@
 // Date planner
 
 // Display current date at top of calendar
-let currentDay = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
+let currentDay = moment().format("dddd, MMMM Do YYYY");
 
 $('#currentDay').text(currentDay);
+
+// Current hour retrieval.
+let currentHour = parseInt(moment().hour());
+// 
+
 // Below are timeblocks for standard business hours.
 // Timeblocks must be color coded for past/present/future time slots
 // Save events into timeblocks, must stay after pages refreshes
 
-function currentTime() {
-    let currentHour = moment().hours();
-    let time = $('.time-block');
-
-    time.each(function(){
-        let hour = parseInt($(this).attr('id'))
-
-
-    })
-}
